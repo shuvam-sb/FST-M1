353e96461df0b7808c50bb3c53bfb776b8f962c0
shuvam-sb <68791041+shuvam-sb@users.noreply.github.com>
2026-01-17 13:01:51 +0530

    Add files via upload
    uploaded Activity 4, updated "LoginSteps.java" file (as per the activity).

--- Cucumber/Activities/LoginSteps.java
+++ Cucumber/Activities/LoginSteps.java
@@ -1,7 +1,9 @@
 package stepDefinitions;
+
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -23,6 +25,19 @@ public class LoginSteps extends BaseClass {
 		userPassword.sendKeys("password");
 	}
 
+	@When(": the user enters {string} and {string}")
+	public void enterCredentialsFromInputs(String username, String password) {
+		// Find the input fields
+		WebElement usernameElement = driver.findElement(By.id("username"));
+		WebElement passwordElement = driver.findElement(By.id("password"));
+		// Clear the fields
+		usernameElement.clear();
+		passwordElement.clear();
+
+		usernameElement.sendKeys(username);
+		passwordElement.sendKeys(password);
+	}
+
 	@And(": clicks the submit button")
 	public void clicks_the_submit_button() {
 		driver.findElement(By.xpath("//button[text()='Submit']")).click();
@@ -30,7 +45,15 @@ public class LoginSteps extends BaseClass {
 
 	@Then(": get the confirmation message and verify it")
 	public void get_the_confirmation_message_and_verify_it() {
+		wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("h2.mt-5"), "Welcome"));
 		String confirmationMessage = driver.findElement(By.cssSelector(".mt-5.text-center")).getText();
-        Assertions.assertEquals("Welcome Back, Admin!", confirmationMessage);
+		Assertions.assertEquals("Welcome Back, Admin!", confirmationMessage);
+	}
+
+	@Then(": get the confirmation text and verify message as {string}")
+	public void confirmMessageAsInput(String expectedMessage) {
+		wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("h2.mt-5"), "Welcome"));
+		String confirmationMessage = driver.findElement(By.cssSelector(".mt-5.text-center")).getText();
+		Assertions.assertEquals(expectedMessage, confirmationMessage);
 	}
 }
