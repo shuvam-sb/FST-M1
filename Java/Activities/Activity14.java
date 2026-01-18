4f8142ee9894e44d7d1b19e4ff093dfd90c1f587
shuvam-sb <68791041+shuvam-sb@users.noreply.github.com>
2026-01-18 15:32:03 +0530

    Updated Activity14.java

--- Java/Activities/Activity14.java
+++ Java/Activities/Activity14.java
@@ -12,28 +12,22 @@ public class Activity14 {
 
         File file = new File("src/main/resources/newfile.txt");
 
-        // Create file if it doesn't exist
         if (file.createNewFile()) {
             System.out.println("File created");
         } else {
             System.out.println("File already exists");
         }
 
-        // Write data into file
         FileUtils.writeStringToFile(file, "Hello File Utils", StandardCharsets.UTF_8);
 
-        // Read file
         String newtext = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
         System.out.println("Data in new file: " + newtext);
 
-        // Create destination directory
         File destDir = new File("src/main/resources/destDir");
         destDir.mkdirs();
 
-        // Copy file
         FileUtils.copyFileToDirectory(file, destDir);
 
-        // Read copied file
         File copiedFile = new File(destDir, "newfile.txt");
         String dirtext = FileUtils.readFileToString(copiedFile, StandardCharsets.UTF_8);
         System.out.println("Data in copied file: " + dirtext);
