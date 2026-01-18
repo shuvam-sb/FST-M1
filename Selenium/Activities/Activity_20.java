e178945186b84bf8afeb0396a2215defc134d11b
shuvam-sb <68791041+shuvam-sb@users.noreply.github.com>
2026-01-18 15:57:21 +0530

    Create Activity_20.java

---
+++ Selenium/Activities/Activity_20.java
@@ -0,0 +1,32 @@
+package examples;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity_20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new FirefoxDriver();
+		   
+        driver.get("https://training-support.net/webelements/alerts");
+        System.out.println("Page title: " + driver.getTitle());
+	
+        WebElement button1 = driver.findElement(By.xpath("(//button[@id='prompt'])"));
+        button1.click();
+        
+        Alert alert=driver.switchTo().alert();
+        System.out.println(alert.getText());
+        alert.sendKeys("Awesome!");
+        alert.accept();
+        
+        WebElement text = driver.findElement(By.xpath("//button[@id='result']"));
+        System.out.println(text.getText());
+        
+        driver.quit(); 
+	}
+
+}
